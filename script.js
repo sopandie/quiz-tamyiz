84d72f129c751d539d16dc75b6e7d904cfc3c904
sopandi <sopandi.salamalazhar@gmail.com>
2026-02-27 13:14:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F6HS71HWC1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-F6HS71HWC1');
-</script>
-
 const board = document.getElementById("board");
 const timerDisplay = document.getElementById("timer");
 const correctDisplay = document.getElementById("correct");
@@ -288,3 +278,12 @@ loadLevels();
 levelSelect.selectedIndex = 0;
 startGame();
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F6HS71HWC1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F6HS71HWC1');
+</script>
