cb4358e5cd13a0a81bd2cf926b8fdb82a1c35837
sopandi <sopandi.salamalazhar@gmail.com>
2026-02-27 13:15:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -278,12 +278,5 @@ loadLevels();
 levelSelect.selectedIndex = 0;
 startGame();
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F6HS71HWC1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-F6HS71HWC1');
-</script>
+
